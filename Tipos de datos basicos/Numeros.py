9fb8cd68c558297fc4ac02fb8f0a203fe2a93dca
celestial987 <delirium987@outlook.com>
2018-05-16 01:45:30 -0400

    Tipos de datos basicos: Numeros

---
+++ Tipos de datos basicos/Numeros.py
@@ -0,0 +1,26 @@
+# Tipos de datos basicos / operaciones
+"""
+suma : 1+1
+resta: 1-1
+Multiplicacion: 2*2
+division: 5/2
+Resto: 5%2
+
+Tipos de numero:
+                Enteros: No tienen parte decima
+                Flotantes: Posen parte decimal se representa con el "." 10.5
+"""
+
+# Variables
+
+n = 10  # Asignamos valo r a una variable
+
+print("Mostramos el valor de N", n)  # La trabajamos Nos muestra su valor
+
+# ejercicio simple
+
+nota_1 = 10
+nota_2 = 5
+nota_media = (nota_1 + nota_2) / 2
+
+print("Mostramos la nota Media", nota_media)
--- hello.py
+++
@@ -1,3 +0,0 @@
-msg = "hello world"
-new_msg = msg.capitalize()
-print (new_msg)
